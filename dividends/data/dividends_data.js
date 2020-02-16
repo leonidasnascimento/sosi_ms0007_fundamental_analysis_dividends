26de7c238c4bba2eb3a06a6dd4c23289b97a9e91
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-02-16 06:08:11 -0300

    Implementation of operational methods

--- dividends/data/dividends_data.js
+++ dividends/data/dividends_data.js
@@ -64,7 +64,52 @@ module.exports = class {
                 on_success(result)
             });
         } catch (error) {
-            console.error(error)
+            on_error(error)
+        }
+    }
+
+    get_dividend_analysis_history(stock_code, on_success, on_error) {
+        try {
+            var query = "EXECUTE [dbo].[Sp_get_hist_dividend_analysis] @stock_code = '" + stock_code + "';"
+
+            execute_query(query, (result) => {
+                on_success(result)
+            });
+        } catch (error) {
+            on_error(error)
+        }
+    }
+
+    set_dividend_analysis(stock_code, company, sector, second_sector, stock_price, stock_type, valuation, stock_available_volume, vol_negotiated_last_21, dividend_last_price, company_net_profit, dividend_yeld, dividend_avg_payout_12_mos, dividend_avg_payout_5_yrs, major_share_holder, company_roe, company_roe_5_yrs, comp_grossdebt_ebtida, dividend_yield_5_yrs, has_dividend_srd_5_yrs, has_dividend_grwth_5_yrs, has_net_profit_reg_5_yrs, on_success, on_error) {
+        try {
+            var query = "EXECUTE [dbo].[SP_INSERT_DIVIDEND_ANALYSIS]"
+            query = query + "  @STOCK_CODE = '" + stock_code + "'"
+            query = query + " ,@COMPANY = '" + company + "'"
+            query = query + " ,@SECTOR = '" + sector + "'"
+            query = query + " ,@SECOND_SECTOR = '" + second_sector + "'"
+            query = query + " ,@STOCK_PRICE = " + stock_price + ""
+            query = query + " ,@STOCK_TYPE = '" + stock_type + "'"
+            query = query + " ,@VALUATION = " + valuation + ""
+            query = query + " ,@STOCK_AVAILABLE_VOLUME = " + stock_available_volume + ""
+            query = query + " ,@VOL_NEGOTIATED_LAST_21 = " + vol_negotiated_last_21 + ""
+            query = query + " ,@DIVIDEND_LAST_PRICE = " + dividend_last_price + ""
+            query = query + " ,@COMPANY_NET_PROFIT = " + company_net_profit + ""
+            query = query + " ,@DIVIDEND_YELD = " + dividend_yeld + ""
+            query = query + " ,@DIVIDEND_AVG_PAYOUT_12_MOS = " + dividend_avg_payout_12_mos + ""
+            query = query + " ,@DIVIDEND_AVG_PAYOUT_5_YRS = " + dividend_avg_payout_5_yrs + ""
+            query = query + " ,@MAJOR_SHARE_HOLDER = '" + major_share_holder + "'"
+            query = query + " ,@COMPANY_ROE = " + company_roe + ""
+            query = query + " ,@COMPANY_ROE_5_YRS = " + company_roe_5_yrs + ""
+            query = query + " ,@COMP_GROSSDEBT_EBTIDA = " + comp_grossdebt_ebtida + ""
+            query = query + " ,@DIVIDEND_YIELD_5_YRS = " + dividend_yield_5_yrs + ""
+            query = query + " ,@HAS_DIVIDEND_SRD_5_YRS = " + has_dividend_srd_5_yrs + ""
+            query = query + " ,@HAS_DIVIDEND_GRWTH_5_YRS = " + has_dividend_grwth_5_yrs + ""
+            query = query + " ,@HAS_NET_PROFIT_REG_5_YRS = " + has_net_profit_reg_5_yrs + ";"
+
+            execute_query(query, (result) => {
+                on_success(result)
+            });
+        } catch (error) {
             on_error(error)
         }
     }
--- dividends/routes/index.js
+++ dividends/routes/index.js
@@ -14,8 +14,141 @@ router.get('/', function (req, res, next) {
   new dividends().get_dividend_analysis(stock_code, (result) => {
     res.status(HttpStatus.OK).type("json").send(JSON.stringify(result))
   }, (error) => {
-    console.error(error)
     res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error)
+  });
+});
+
+router.get('/dividend_history', function (req, res, next) {
+  var stock_code = ""
+
+  if ((Object.keys(req.query).length > 0) || (Object.keys(req.query).indexOf("code") > 0)) {
+    stock_code = req.query["code"]
+  }else{
+    res.status(HttpStatus.BAD_REQUEST).type("json").send(JSON.stringify("Stock code is required"))
+  }
+
+  new dividends().get_dividend_analysis_history(stock_code, (result) => {
+    res.status(HttpStatus.OK).type("json").send(JSON.stringify(result))
+  }, (error) => {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error)
+  });
+});
+
+router.post('/', function (req, res, next) {
+  if (!("stock_code" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'stock_code' is required");
+    return;
+  }
+
+  if (!("company" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'company' is required");
+    return;
+  }
+
+  if (!("sector" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'sector' is required");
+    return;
+  }
+
+  if (!("second_sector" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'second_sector' is required");
+    return;
+  }
+
+  if (!("stock_price" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'stock_price' is required");
+    return;
+  }
+
+  if (!("stock_type" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'stock_type' is required");
+    return;
+  }
+
+  if (!("valuation" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'valuation' is required");
+    return;
+  }
+
+  if (!("stock_available_volume" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'stock_available_volume' is required");
+    return;
+  }
+
+  if (!("vol_negotiated_last_21" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'vol_negotiated_last_21' is required");
+    return;
+  }
+
+  if (!("dividend_last_price" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'dividend_last_price' is required");
+    return;
+  }
+
+  if (!("company_net_profit" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'company_net_profit' is required");
+    return;
+  }
+
+  if (!("dividend_yeld" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'dividend_yeld' is required");
+    return;
+  }
+
+  if (!("dividend_avg_payout_12_mos" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'dividend_avg_payout_12_mos' is required");
+    return;
+  }
+
+  if (!("dividend_avg_payout_5_yrs" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'dividend_avg_payout_5_yrs' is required");
+    return;
+  }
+
+  if (!("major_share_holder" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'major_share_holder' is required");
+    return;
+  }
+
+  if (!("company_roe" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'company_roe' is required");
+    return;
+  }
+
+  if (!("company_roe_5_yrs" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'company_roe_5_yrs' is required");
+    return;
+  }
+
+  if (!("comp_grossdebt_ebtida" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'comp_grossdebt_ebtida' is required");
+    return;
+  }
+
+  if (!("dividend_yield_5_yrs" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'dividend_yield_5_yrs' is required");
+    return;
+  }
+
+  if (!("has_dividend_srd_5_yrs" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'has_dividend_srd_5_yrs' is required");
+    return;
+  }
+
+  if (!("has_dividend_grwth_5_yrs" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'has_dividend_grwth_5_yrs' is required");
+    return;
+  }
+
+  if (!("has_net_profit_reg_5_yrs" in req.body)) {
+    res.status(HttpStatus.BAD_REQUEST).send("Field 'has_net_profit_reg_5_yrs' is required");
+    return;
+  }
+
+  new dividends().set_dividend_analysis(req.body.stock_code, req.body.company, req.body.sector, req.body.second_sector, req.body.stock_price, req.body.stock_type, req.body.valuation, req.body.stock_available_volume, req.body.vol_negotiated_last_21, req.body.dividend_last_price, req.body.company_net_profit, req.body.dividend_yeld, req.body.dividend_avg_payout_12_mos, req.body.dividend_avg_payout_5_yrs, req.body.major_share_holder, req.body.company_roe, req.body.company_roe_5_yrs, req.body.comp_grossdebt_ebtida, req.body.dividend_yield_5_yrs, req.body.has_dividend_srd_5_yrs, req.body.has_dividend_grwth_5_yrs, req.body.has_net_profit_reg_5_yrs, (result) => {
+    res.status(HttpStatus.OK).type("json").send(JSON.stringify(result))
+  }, (error) => {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).type("json").send(JSON.stringify(error))
   });
 });
 
